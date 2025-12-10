372ea5f2f06e2211beaa2ac4f9c77457ec345b5f
Dewa <dewa.a.putra@sat.co.id>
2025-12-10 10:41:06 +0700

    BuildWithAngga - Go Essentials for Absolute Beginner (Rest API Server) ~ Validation Error API

--- restapiserver/dto/create_book.go
+++ restapiserver/dto/create_book.go
@@ -3,11 +3,11 @@ package dto
 // data transfer object (dto)
 
 type CreateBookRequest struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	ISBN        string `json:"isbn"`
-	Author      string `json:"author"`
-	Genre       string `json:"genre"`
-	Stock       int    `json:"stock"`
-	PublishDate string `json:"publish_date"`
+	Title       string `json:"title" validate:"required"`
+	Description string `json:"description" validate:"required"`
+	ISBN        string `json:"isbn" validate:"required"`
+	Author      string `json:"author" validate:"required"`
+	Genre       string `json:"genre" validate:"required"`
+	Stock       int    `json:"stock" validate:"min=1"`
+	PublishDate string `json:"publish_date" validate:"required"`
 }
